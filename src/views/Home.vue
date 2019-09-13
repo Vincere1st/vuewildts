9e664452c6f53e094dbf0eb77591577a888929c6
Vincent Ostyn <vincent.ostyn@gfi.fr>
2019-09-13 14:31:08 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <div class="nav">
+            <span v-for="tag in tags">
+                <input type="checkbox" v-model="tagCkecked"><label :class={[tag.color]:true,striketrought:!tagChecked}>{{ tag.title }}</label>
+            </span>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {Component, Vue} from 'vue-property-decorator';
+    import data from '../data';
+    // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            // HelloWorld
+        },
+    })
+    export default class Home extends Vue {
+        dreamTeam = data.dreamTeam
+        columns = data.columns
+        tags = data.tags
+        tickets = data.tickets
+    }
 </script>
+
+<style lang="scss" scoped>
+    .grey {
+        color: darkgrey;
+    }
+    .green {
+        color: darkgreen;
+    }
+    .orange {
+        color: darkorange;
+    }
+    .red {
+        color: darkred;
+    }
+    .purple {
+        color: darkviolet;
+    }
+    .blue {
+        color: darkblue;
+    }
+</style>
