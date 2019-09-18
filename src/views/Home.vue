413249d1dd2f991fb7a328d4de2bd647f6e51f23
Vincent Ostyn <vincent.ostyn@gfi.fr>
2019-09-18 17:00:30 +0200

    add filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,71 +2,121 @@
     <div class="home">
         <div class="nav">
             <span v-for="(tag, key) in tags" :key="key">
-                <input type="checkbox" v-model="tagsChecked[key]"><label :class="{ [tag.color]: true, striketrought: !tagsChecked[key] }">{{ tag.title }}</label>
+                <input type="checkbox" v-model="tagsChecked[key]"><label
+                    :class="{ [tag.color]: true, striketrought: !tagsChecked[key] }">{{ tag.title }}</label>
             </span>
         </div>
-        <affectations :columns="columns" :tickets="tickets" :tags="tags" :showtickets="showTickets"></affectations>
+        <affectations :columns="columns" :tickets="ticketsWithTagsSelected()" :tags="tags"></affectations>
     </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import data from '../data';
-import Affectations from "@/components/Affectations.vue";
+    import {Component, Vue} from 'vue-property-decorator';
+    import data from '../data';
+    import Affectations from "@/components/Affectations.vue";
+    import index from "vue-router/src/index";
 
-@Component({
-    components: {
-        Affectations
-    },
-})
-export default class Home extends Vue {
-    dreamTeam = data.dreamTeam
-    columns = data.columns
-    tags = data.tags
-    tickets = data.tickets
+    @Component({
+            components: {
+                Affectations
+            },
+        },
+    )
 
-    tagsChecked:any = {}
+    export default class Home extends Vue {
+        dreamTeam = data.dreamTeam
+        columns = data.columns
+        tags = data.tags
+        tickets = data.tickets
+        tagsChecked: any = {}
 
-    created () {
-        this.tagsChecked = this.buildDefaultTagsCheck()
-    }
-
-    buildDefaultTagsCheck() {
-        let defaultTagChecked: any = {}
-        for (const tag in this.tags) {
-            defaultTagChecked[tag] = true
-            Object.assign(defaultTagChecked, {tag: true})
+        created() {
+            this.tagsChecked = this.buildDefaultTagsCheck()
         }
-        return defaultTagChecked
-    }
 
-    showTickets() {
-        console.log('tata')
-        // for (const tag in this.tags){
-        //     if (this.buildDefaultTagsCheck[tag] === true) {
-        //         console.log('toto')
-        //     }
-        // }
+        beforeUpdate() {
+            this.ticketsWithTagsSelected()
+        }
+
+        buildDefaultTagsCheck() {
+            let defaultTagChecked: any = {}
+            for (const tag in this.tags) {
+                defaultTagChecked[tag] = true
+            }
+            return defaultTagChecked
+        }
+
+        tagsSelected() {
+            const tagsSelected = []
+            for (const tags in this.tagsChecked) {
+                if (this.tagsChecked[tags] === true) {
+                    tagsSelected.push(parseInt(tags))
+                }
+            }
+            return tagsSelected
+        }
+
+        ticketsWithTagsSelected() {
+            // return Object.values(this.tickets).filter((ticket) => {
+            //     for (const tag of ticket.tags) {
+            //         if (this.tagsSelected().includes(tag)){
+            //             return true
+            //         }
+            //         return false
+            //     }
+            // })
+            let ticketWithIndex = this.tickets
+                for (const index in ticketWithIndex) {
+                    ticketWithIndex[index].index = parseInt(index)
+                }
+
+            let ticketsFilter = []
+                for (const ticket of Object.values(ticketWithIndex)) {
+                    for (const tag of ticket.tags) {
+                        console.log(tag +' tag')
+                        if (this.tagsSelected().includes(tag)) {
+                            console.log(ticket.index + ' index')
+                            ticketsFilter.push(ticket)
+                        }
+                    }
+                }
+
+
+            // for (const tag of this.tagsSelected()) {
+            //     console.log(tag)
+            //     for (const ticket of Object.values(this.tickets)) {
+            //         console.log(ticket.tags)
+            //         if (ticket.tags.includes(tag)){
+            //             ticketsFilter.push(ticket)
+            //         }
+            //     }
+            // }
+            return ticketsFilter
+        }
     }
-}
 </script>
 
 <style lang="scss">
     .grey {
         color: darkgrey;
     }
+
     .green {
         color: darkgreen;
     }
+
     .orange {
         color: darkorange;
     }
+
     .red {
         color: darkred;
     }
+
     .purple {
         color: darkviolet;
     }
+
     .blue {
         color: darkblue;
     }
